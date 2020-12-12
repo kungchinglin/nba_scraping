3298d680f0879bf0cbcf8144c7c3043318309af1
Kung-Ching Lin <kung.ching.lin@gmail.com>
2020-12-12 22:05:10 +0800

    Finished the clustering program

--- player_cluster.py
+++ player_cluster.py
@@ -32,7 +32,7 @@ player_trad_plus_award_new_stmt = '''SELECT * from Players P
                                     where T.Year between {year_start} and {year_end} '''
 
 
-year_start = 2010
+year_start = 1980
 year_end = 2019
 
 #df_player_trad = pd.read_sql_query(player_trad_stmt.format(year_start = year_start, year_end = year_end), conn)
@@ -54,7 +54,7 @@ def GetMPG(row):
 #We only look at players who play more than 10 mpg and 10 games.
 
 def DF_stat_preprocess(df):
-    df2 = df.drop(columns =['id','Pos'], inplace = False)
+    df2 = df.drop(columns =['id'], inplace = False)
     df2['MPG'] = df2.apply(GetMPG, axis = 1)
     df2 = df2[df2['MPG']>10]
     df2 = df2[df2['G']>10]
@@ -65,13 +65,15 @@ df_player_trad = DF_stat_preprocess(df_player_trad)
 df_player_adv = DF_stat_preprocess(df_player_adv)
 
 
+
 #For players traded mid-season, I suppose it makes sense to NOT combine their stats. It can be argued that the player has similar impact as the star under certain circumstances.
 
 
 #For traditional stats, we look at per-36. For advanced, we should not look at total winshare. Also, we can drop box plus-minus because it is described by OBPM + DBPM.
 
-df_player_adv.drop(columns = ['WS', 'BPM','OWS','DWS'], inplace = True)
+df_player_adv.drop(columns = ['WS', 'BPM'], inplace = True)
 df_player_adv['OWS'] /= df_player_adv['MP']
+df_player_adv['DWS'] /= df_player_adv['MP']
 
 
 
@@ -96,22 +98,14 @@ df_player_trad.fillna(0,inplace=True)
 df_player_adv.replace([np.inf,'inf'], np.nan, inplace = True)
 df_player_adv.fillna(0,inplace=True)
 
-df_player_trad.drop(columns = ['Age','G','GS','MPG','PTS','TRB','AST','STL','BLK','TOV','PF','MP'],inplace = True)
+df_player_trad.drop(columns = ['Age','G','GS','PTS','TRB','AST','STL','BLK','TOV','PF','MP'],inplace = True)
 
-df_player_adv.drop(columns = ['G','MP'], inplace = True)
+df_player_adv.drop(columns = ['Age','G','MP'], inplace = True)
 
 
 df_player_trad['FTP'] = pd.to_numeric(df_player_trad['FTP'])
 df_player_trad['ThreePP'] = pd.to_numeric(df_player_trad['ThreePP'])
 
-
-
-print(df_player_trad.columns.values)
-
-print(df_player_trad.min())
-
-print(df_player_adv.columns.values)
-print(df_player_adv.min())
 
 
 #=============================================
@@ -125,22 +119,35 @@ def Normalize_and_drop(df):
     Team_ids = df['Team_id']
     Player_ids = df['Player_id']
     Years = df['Year']
+    Positions = df['Pos']
 
-    df_nor = df.drop(columns = ['Year','Player_name','Team_id','Player_id'])
+    df_nor = df.drop(columns = ['Year','Player_name','Team_id','Player_id', 'MPG','Pos'])
 
     df_nor = (df_nor-df_nor.min())/(df_nor.max()-df_nor.min())
     df_nor['Player_name'] = Player_names
     df_nor['Team_id'] = Team_ids
     df_nor['Player_id'] = Player_ids
     df_nor['Year'] = Years
+    df_nor['Pos'] = Positions
 
     return df_nor
 
 df_player_adv_nor = Normalize_and_drop(df_player_adv)
 df_player_trad_nor = Normalize_and_drop(df_player_trad)
 
-print(df_player_trad_nor.head())
-print(df_player_adv_nor.head())
+
+#Let's get the data of Lebron James and Kevin Durant.
+
+def FindID(Player_name):
+
+    cur = conn.cursor()
+
+    cur.execute(''' SELECT id from Players where Player_name = ? ''', (Player_name,))
+
+    id = cur.fetchone()
+
+
+    return id
 
 
 
@@ -148,105 +155,67 @@ print(df_player_adv_nor.head())
 
 
 
+def Calculate_distance(df, player_name, year):
+
+    ID = FindID(player_name)
+    if ID == None:
+        ID = FindID(player_name+"*")
+        if ID == None:
+            print("Can't find the player.")
+            return
+    
+    player_df = df[df['Player_id'] == ID]
+    #print(player_df)
+    player_year = player_df[player_df['Year'] == year].squeeze()
+
+    if player_year.empty:
+        print("The player didn't play that year!")
+        return
+
+    Player_names = df['Player_name']
+    Years = df['Year']
+    Positions = df['Pos']
+
+    df_num = df.drop(columns = ['Year','Player_name','Team_id','Player_id','Pos'])
 
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.preprocessing import normalize, StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_validate
-
-scaler = StandardScaler()
-
-LR = LogisticRegression()
-
-Trad_stat = ["Age", "MPG", "PTS/G", "RB/G", "AST/G", "STL/G", "BLK/G", "BLK/G", "PF/G"]
-X = df_player_trad[Trad_stat]
-scaler.fit(X)
-X = scaler.transform(X)
-y = df_player_trad["Award"]
-y = y.astype('int')
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+    row_num = player_year.drop(labels = ['Year','Player_name','Team_id','Player_id', 'Pos'])
 
 
-LR.fit(X_train,y_train)
 
-coef = LR.coef_
-coef = coef[0]
-ind = sorted(range(len(coef)), key = lambda k: abs(coef[k]), reverse=True)
+    df_num = df_num - row_num
 
+    df_num['Distance'] = df_num.apply(np.linalg.norm, axis = 1)
+    df_num['Player_name'] = Player_names
+    #df_num['Team_id'] = Team_ids
+    #df_num['Player_id'] = Player_ids
+    df_num['Year'] = Years
+    df_num['Pos'] = Positions
 
-imp_list = [Trad_stat[i] for i in ind]
-
-print("LR test for current year prediction based on per-game stat is",LR.score(X_test, y_test))
-print("The importance of coefficients are,", ', '.join(imp_list))
-
-LR2 = LogisticRegression()
-Trad_stat_36 = ["Age", "MPG", "PTS/36", "RB/36", "AST/36", "STL/36", "BLK/36", "BLK/36", "PF/36"]
-X2 = df_player_trad[Trad_stat_36]
-scaler.fit(X2)
-X2 = scaler.transform(X2)
-y2 = df_player_trad["Award"]
-y2 = y2.astype('int')
+    df_dist = df_num[['Distance','Player_name','Pos','Year']].sort_values(by = ['Distance'])
 
 
-X_train, X_test, y_train, y_test = train_test_split(X2, y2, test_size=0.10, random_state=42)
+    print("The closest normalized performance for Player {} in Year {} is as follows:".format(player_name,year))
+    print(df_dist.head(30))
 
-LR2.fit(X_train,y_train)
-
-coef = LR2.coef_
-coef = coef[0]
-ind = sorted(range(len(coef)), key = lambda k: abs(coef[k]), reverse=True)
+    #return df_dist  
 
 
-imp_list = [Trad_stat_36[i] for i in ind]
 
-print("LR test for current year prediction based on per-36 stat is",LR2.score(X_test, y_test))
-print("The importance of coefficients are,", ', '.join(imp_list))
+#Calculate_distance(df_player_trad_nor, "LeBron James", 2010)
+#Calculate_distance(df_player_adv_nor, "Vince Carter", 2017)
 
-LR3 = LogisticRegression()
+while True:
+    name = input("Input the player you want to cluster with: ")
+    if name == '':
+        print('Ending the program...')
+        break
 
-X3 = df_player_trad[Trad_stat]
-scaler.fit(X3)
-X3 = scaler.transform(X3)
-y3 = df_player_trad["Award_next_year"]
-y3 = y3.astype('int')
-
-X_train, X_test, y_train, y_test = train_test_split(X3, y3, test_size=0.10, random_state=42)
+    year = int(input("Input the year you want to set as the baseline: "))
 
 
-LR3.fit(X_train,y_train)
-
-coef = LR3.coef_
-coef = coef[0]
-ind = sorted(range(len(coef)), key = lambda k: abs(coef[k]), reverse=True)
 
 
-imp_list = [Trad_stat[i] for i in ind]
 
+    Calculate_distance(df_player_adv_nor, name, year)
 
-print("LR test for next year prediction based on per-game stat is",LR3.score(X_test, y_test))
-print("The importance of coefficients are,", ', '.join(imp_list))
-
-LR4 = LogisticRegression()
-X4 = df_player_trad[Trad_stat_36]
-scaler.fit(X4)
-X4 = scaler.transform(X4)
-y4 = df_player_trad["Award_next_year"]
-y4 = y4.astype('int')
-
-X_train, X_test, y_train, y_test = train_test_split(X4, y4, test_size=0.10, random_state=42)
-
-LR4.fit(X_train,y_train)
-
-coef = LR4.coef_
-coef = coef[0]
-ind = sorted(range(len(coef)), key = lambda k: abs(coef[k]), reverse=True)
-
-
-imp_list = [Trad_stat_36[i] for i in ind]
-
-
-print("LR test for next year prediction based on per-36 stat is",LR4.score(X_test, y_test))
-print("The importance of coefficients are,", ', '.join(imp_list))
